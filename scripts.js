b89630d6fa2127a1d1501303367116a86efec69f
academy-dl <directlineacademy@gmail.com>
2020-12-24 20:27:06 +0400

    init

---
+++ scripts.js
@@ -0,0 +1,1 @@
+"use strict";var e=document.querySelector(".mobile-header_js"),t=document.querySelector(".header__mobile-button_js"),o=document.querySelector(".mobile-header__close-button_js");t.addEventListener("click",function(){e.classList.add("mobile-header_open")}),o.addEventListener("click",function(){e.classList.remove("mobile-header_open")});
